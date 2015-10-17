d2210eef5dc3b08be28ebe7fa660eb02d54caded
Dmytro G. Sergiienko <sergiienko.d.g@gmail.com>
2015-10-17 23:03:28 +0300

    Menu bugfixes

--- js/scroll-menu.js
+++ js/scroll-menu.js
@@ -1,20 +1,24 @@
-var $header = $("#header"),
-	hClass = "ba-header_secondary",
-	pos = parseFloat( $(".ba-banner").css("height") ) - 1;
-$(document).scroll(function() {
-	if($(this).scrollTop() > pos) {
-		if (!$header.hasClass(hClass)) {
-			$header
-				.addClass("ba-header_secondary")
-				.addClass("ba-header_shown")
+;(function($){
+
+	var $header = $("#header"),
+		hClass = "ba-header_secondary",
+		pos = parseFloat( $(".ba-banner").css("height") ) - 1;
+	$(document).scroll(function() {
+		if($(this).scrollTop() > pos) {
+			if (!$header.hasClass(hClass)) {
+				$header
+					.addClass("ba-header_secondary")
+					.addClass("ba-header_shown")
+			}
+		} else {
+			if ($header.hasClass(hClass)) {
+				$header
+					.removeClass("ba-header_shown")
+				setTimeout(function() {
+					$header.removeClass(hClass);
+				}, 300);
+			}
 		}
-	} else {
-		if ($header.hasClass(hClass)) {
-			$header
-				.removeClass("ba-header_shown")
-			setTimeout(function() {
-				$header.removeClass(hClass);
-			}, 300);
-		}
-	}
-});
+	});
+
+})(jQuery);
