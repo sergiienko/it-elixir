32e42a11095c22d28e7b815e1a454d324aa810f0
Dmytro G. Sergiienko <sergiienko.d.g@gmail.com>
2015-10-23 13:45:40 +0300

    Bugfixes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,66 @@
 ;(function($) {
 
-	$(function() {
+  $(function() {
 
-		$(".ba-menu-toggle").click(function() {
+    $(".ba-menu-toggle").click(function() {
 
-			$(document.body).toggleClass("ba-menu-open");
-		});
+      $(document.body).toggleClass("ba-menu-open");
+    });
 
-		$(".ba-menu-overlay").click(function() {
+    $(".ba-menu-overlay").click(function() {
 
-			$(document.body).toggleClass("ba-menu-open");
-		});
-	});
+      $(document.body).toggleClass("ba-menu-open");
+    });
+
+    // Slider
+
+    $(".ba-projects__slider").slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      prevArrow: ".slick-prev",
+      nextArrow: ".slick-next",
+      slide: ".ba-slide",
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+    // Map
+
+    var display = new google.maps.LatLng(50.420561, 30.519305),
+        initialize = function() {
+          var mapProp = {
+                center: new google.maps.LatLng(50.420852, 30.520927),
+                zoom: 17,
+                disableDefaultUI: true,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+              },
+
+              map = new google.maps.Map(document.getElementById("map"), mapProp);
+              
+              marker = new google.maps.Marker({
+                  position: display,
+                  icon: "img/icon/address.svg"
+              });
+
+          marker.setMap(map);
+        }
+
+    google.maps.event.addDomListener(window, "load", initialize);
+
+  });
 
 })(jQuery);
--- js/scroll-menu.js
+++ js/scroll-menu.js
@@ -1,38 +1,38 @@
-;(function($){
+;(function($) {
 
-	var $header = $("#header"),
-		hClass = "ba-header_secondary",
-		pos = parseFloat( $(".ba-banner").css("height") ) - 1;
+  var $header = $("#header"),
+    hClass = "ba-header_secondary",
+    pos = parseFloat( $(".ba-banner").css("height") ) - 1;
 
-	$(document).scroll(function() {
-		if($(this).scrollTop() > pos) {
-			if (!$header.hasClass(hClass)) {
-				$header
-					.addClass("ba-header_secondary")
-					.addClass("ba-header_shown")
-					.find(".ba-logo__img")
-						.attr("src", "img/logo-middle.svg");
+  $(document).scroll(function() {
+    if($(this).scrollTop() > pos) {
+      if (!$header.hasClass(hClass)) {
+        $header
+          .addClass("ba-header_secondary")
+          .addClass("ba-header_shown")
+          .find(".ba-logo__img")
+            .attr("src", "img/logo-middle.svg");
 
-				if ($(".ba-menu-toggle").css("display") !== "none") {
-					$(".ba-nav").prependTo($(document.body));
-				}
-			}
-		} else {
-			if ($header.hasClass(hClass)) {
-				$header
-					.removeClass("ba-header_shown")
-				setTimeout(function() {
-					$header
-						.removeClass(hClass)
-						.find(".ba-logo__img")
-							.attr("src", "img/logo.svg");
+        if ($(".ba-menu-toggle").css("display") !== "none") {
+          $(".ba-nav").prependTo($(document.body));
+        }
+      }
+    } else {
+      if ($header.hasClass(hClass)) {
+        $header
+          .removeClass("ba-header_shown")
+        setTimeout(function() {
+          $header
+            .removeClass(hClass)
+            .find(".ba-logo__img")
+              .attr("src", "img/logo.svg");
 
-					if ($(".ba-menu-toggle").css("display") !== "none") {
-						$header.find(".ba-logo").after($(".ba-nav"));
-					}
-				}, 300);
-			}
-		}
-	});
+          if ($(".ba-menu-toggle").css("display") !== "none") {
+            $header.find(".ba-logo").after($(".ba-nav"));
+          }
+        }, 300);
+      }
+    }
+  });
 
 })(jQuery);
