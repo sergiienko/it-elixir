9d163f5c99f4ef0ea1432fd29eba55fab31a6d61
Dmytro G. Sergiienko <sergiienko.d.g@gmail.com>
2015-11-11 13:18:00 +0200

    Disabled scroll wheel for map

--- js/main.js
+++ js/main.js
@@ -54,7 +54,8 @@
                 center: new google.maps.LatLng(50.420852, 30.520927),
                 zoom: 17,
                 disableDefaultUI: true,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                scrollwheel: false
               },
 
               map = new google.maps.Map(document.getElementById("map"), mapProp);
